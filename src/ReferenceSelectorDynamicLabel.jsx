7106224fca9339aba8d91b5ad5daf59e44ff4be8
Johan Munneke <johanmunneke94@gmail.com>
2022-07-15 16:47:25 +0200

    Setup project and widget

--- src/ReferenceSelectorDynamicLabel.jsx
+++ src/ReferenceSelectorDynamicLabel.jsx
@@ -4,7 +4,22 @@ import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/ReferenceSelectorDynamicLabel.css";
 
 export class ReferenceSelectorDynamicLabel extends Component {
+
+    getDropdown () {
+        if (this.props.objectsDatasource.status === 'available') {
+            let optionList = this.props.objectsDatasource.items.map( object => {
+                return <option>{this.props.label.get(object).value}</option>
+            });
+            return <select className={'form-control'}>{optionList}</select>;
+        } else {
+            return undefined;
+        }
+    }
+
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+
+        return <div style={this.props.style}
+        className={'mx-compound-control ' + (this.props.class ? this.props.class : '')}
+        tabIndex={this.props.tabIndex}>{this.getDropdown()}</div>;
     }
 }
