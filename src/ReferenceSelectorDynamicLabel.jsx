1a9882c9e475ee45622347ee1923e4f96332eaaa
Johan Munneke <johanmunneke94@gmail.com>
2022-10-21 10:15:15 +0200

    First version with basics

--- src/ReferenceSelectorDynamicLabel.jsx
+++ src/ReferenceSelectorDynamicLabel.jsx
@@ -1,25 +1,49 @@
-import { Component, createElement } from "react";
+import { createElement } from "react";
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/ReferenceSelectorDynamicLabel.css";
 
-export class ReferenceSelectorDynamicLabel extends Component {
+export function ReferenceSelectorDynamicLabel(props) {
 
-    getDropdown () {
-        if (this.props.objectsDatasource.status === 'available') {
-            let optionList = this.props.objectsDatasource.items.map( object => {
-                return <option>{this.props.label.get(object).value}</option>
+    const onChange = (e) => {
+        console.warn(e.target.value);
+        let target = undefined;
+        if (e.target.value !== "placeholder") {
+            target = props.objectsDatasource.items[e.target.value];
+        }
+        props.reference.setValue(target);
+        if (props.onChangeAction && props.onChangeAction.canExecute) {
+            props.onChangeAction.execute();
+        }
+    }
+
+    const getDropdown = () => {
+        if (props.objectsDatasource.status === 'available') {
+            let optionList = props.objectsDatasource.items.map((object, index) => {
+                return <option value={index}>{props.label.get(object).value}</option>
             });
-            return <select className={'form-control'}>{optionList}</select>;
+            return <select className={'form-control'}
+                onChange={(e) => onChange(e)}>
+                <option value= "placeholder"
+                    className="emptyOption">
+                    {props.emptyCaption.value}
+                </option>
+                {optionList}
+            </select>;
         } else {
             return undefined;
         }
     }
-
-    render() {
-
-        return <div style={this.props.style}
-        className={'mx-compound-control ' + (this.props.class ? this.props.class : '')}
-        tabIndex={this.props.tabIndex}>{this.getDropdown()}</div>;
-    }
+    /* todo
+    validation
+    plaatje
+    readme
+    lees associatie
+    verander als in mendix veranderd
+    refresh opties als in mendix veranderd
+    styling placeholder
+    */
+    return (<div style={props.style}
+        className={'mx-compound-control ' + (props.class ? props.class : '')}
+        tabIndex={props.tabIndex}> {getDropdown()}</div>);
 }
+
